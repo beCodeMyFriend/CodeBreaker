8153f774cd78fdbd9ca59b28e4addf7a7629bbd9
xaviuzz <xaviuzz@prometeuzzzz.(none)>
2012-05-25 17:41:31 +0200

    code generation

--- js/src/CB.js
+++ js/src/CB.js
@@ -9,8 +9,27 @@ CB.Services = CB.Services || {};
 CB.Renderers = CB.Renderers || {};
 CB.Decorations = CB.Decorations || {};
 
-CB.generate = function() {
-    return ["red", "cyan", "orange", "violet"];
+CB.generate = function(code) {
+    var aCode = code || [];
+    if (aCode.length == 4) return aCode;
+    var aColor = CB._randomColor();
+    if (CB._notContained(aColor, aCode)) {
+        aCode.push(aColor);
+    }
+    CB.generate(aCode);
+    return aCode;
 };
 
 CB.Colors = ["red", "cyan", "orange", "violet", "green", "yellow"];
+
+CB._randomColor = function() {
+    var index = Math.floor(Math.random() * (6));
+    return CB.Colors[index];
+};
+
+CB._notContained = function(aColor, aCode) {
+    for (var i = 0; i < aCode.length; i++) {
+        if (aCode[i] == aColor) return false;
+    }
+    return true;
+};
