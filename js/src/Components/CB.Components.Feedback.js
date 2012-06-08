37d8687751b163065fb28a69d9d2e75ec27beea8
xaviuzz <xaviuzz@prometeuzzzz.(none)>
2012-06-08 10:35:10 +0200

    Feedback shows the answer

--- js/src/Components/CB.Components.Feedback.js
+++ js/src/Components/CB.Components.Feedback.js
@@ -3,12 +3,17 @@ CB.Components.Feedback = CUORE.Class(CUORE.Components.List, {
     init: function() {
         CB.Components.Feedback.parent.init.call(this);
         this.renderer = new CB.Renderers.Feedback();
-        this.addHandler("CODE_generate_EXECUTED",new CUORE.Handlers.Executor("showCodeGenerated"));
+        this.addExecHandler("CODE_generate_EXECUTED","showCodeGenerated");
+        this.addExecHandler("CODE_check_EXECUTED","showResponse");
     },
 
     showCodeGenerated: function(Message) {
         this.list.push("Code generated");
         this.updateRender();
-    }
+    },
     
+    showResponse: function(Message) {
+        this.list.push("a validation");
+        this.updateRender();
+    }
 });
--- js/test/Components/CB.Components.Feedback.Spec.js
+++ js/test/Components/CB.Components.Feedback.Spec.js
@@ -16,6 +16,15 @@ describe("CB.Feedback", function() {
         aComponent.size().should.be(1);
     });
     
+    it("Handles validation of codes inserting a message in the list", function() {
+        var aMessage= new CUORE.Message();
+        aMessage.putOnAnswer("result","XXX**");
+        aComponent.getManagedEvents().should.include('CODE_check_EXECUTED');
+        aComponent.eventDispatch('CODE_check_EXECUTED',aMessage);
+        aComponent.size().should.be(1);
+        
+    });
+    
     it("has its own renderer", function() {
         aComponent.renderer.should.be.an.instanceOf(CB.Renderers.Feedback);
     });    
